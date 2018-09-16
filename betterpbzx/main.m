637212d98553b4aaec3b139031b98573ec4da612
Ahmad Nagy <kjcsrlcofounder@Ahmads-iMac.local>
2018-09-16 15:45:59 +0200

    Initial Commit

---
+++ betterpbzx/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  betterpbzx
+//
+//  Created by Ahmad Nagy on 9/16/18.
+//  Copyright © 2018 Ahmad Nagy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
